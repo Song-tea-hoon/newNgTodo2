6cf89b80cdd10caffc3d0745503e0363f2ef7c9a
song <song@song.com>
2017-12-09 10:57:31 +0900

    라우팅메뉴 연결

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {HomeComponent} from './home/home.component';
+import {JqueryComponent} from './jquery/jquery.component';
+import {AngularComponent} from './angular/angular.component';
+
+const routes: Routes = [
+  {
+    path: '', component: IndexComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'jquery', component: JqueryComponent},
+      {path: 'angular', component: AngularComponent},
+    ]
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
