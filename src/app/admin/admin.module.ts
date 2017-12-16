77dcf2ece444b3d765fe9eb15fcbe44e56fe2eb1
song <song@song.com>
2017-12-16 17:54:54 +0900

    admin new list 가져오기

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
+import {AdminRoutingModule} from './admin-routing';
+import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {AdminService} from './admin.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
-  declarations: [AdminComponent, HomeComponent, NewsComponent]
+  declarations: [AdminComponent, HomeComponent, NewsComponent],
+  providers:[AdminService]
 })
 export class AdminModule { }
--- src/app/admin/news/news.component.ts
+++ src/app/admin/news/news.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AdminService} from '../admin.service';
+import {NewsVO} from '../../domain/news.vo';
+import {ResultVO} from '../../domain/result.vo';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
+  newsList = new Array<NewsVO>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private adminService: AdminService) {
   }
 
+  ngOnInit() {
+    this.findNews();
+  }
+
+  findNews() {
+    const params = {
+      start_index: 0,
+      page_size: 5
+    };
+
+    this.adminService.findNews(params)
+      .subscribe((res: ResultVO) => {
+        this.newsList = res.data;
+        console.log(this.newsList);
+      });
+  }
 }
